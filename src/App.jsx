296171b52aaa603a34c24b1f0ba233efe8dbd386
hggnomm <hggnomm.dev@gmail.com>
2025-02-15 00:43:33 +0700

    Change layout to test cicd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="flex justify-center items-center w-full h-screen">
+      <h1 className="text-3xl text-red-400">App</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
